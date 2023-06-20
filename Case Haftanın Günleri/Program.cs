707d24d4ef77fbb2b2d9f94fe30dda52d363ee70
batuh <batuh@BATU-PC>
2023-06-20 20:11:55 +0300

    Add project files.

---
+++ Case Haftanın Günleri/Program.cs
@@ -0,0 +1,32 @@
+﻿// Günü yazınca Haftanın kaçıncı günü olduğunu söylüyor
+Console.Write("Gün adı giriniz: ");
+string GunAd = Console.ReadLine().ToLower();
+switch (GunAd)
+{ 
+    case "pazartesi":
+        Console.WriteLine("Haftanın 1. Günü");
+        break;
+    case "salı": 
+        Console.WriteLine("Haftanın 2. Günü"); 
+        break;
+    case "çarşamba": 
+        Console.WriteLine("Haftanın 3. Günü"); 
+        break;
+    case "perşembe": 
+        Console.WriteLine("Haftanın 4. Günü"); 
+        break;
+    case "cuma": 
+        Console.WriteLine("Haftanın 5. Günü"); 
+        break;
+    case "cumartesi": 
+        Console.WriteLine("Haftanın 6. Günü"); 
+        break;
+    case "pazar": 
+        Console.WriteLine("Haftanın 7. Günü"); 
+        break;
+    default: Console.WriteLine("Böyle bir gün var mı sence(soruyorum sadece)"); 
+        break;
+}    
+
+
+
